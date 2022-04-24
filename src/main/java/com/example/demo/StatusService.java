9793f3c15b65662756408eb4775029664f199856
Brendan James Mulhern <brendanjamesmulhern@gmail.com>
2022-04-24 12:23:46 -0400

    first commit

---
+++ src/main/java/com/example/demo/StatusService.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatusService {
+
+    public String processStatus(boolean arriving) {
+        if (arriving) {
+            return "hello";
+        } else {
+            return "goodbye";
+        }
+    };
+}
